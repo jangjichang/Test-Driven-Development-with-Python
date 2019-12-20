8ff9631559f0570ea5b02fd8da4c65d98b2e7ad4
JichangJang <wlckd90@gmail.com>
2019-12-20 18:11:37 +0900

    functional_tests 앱 생성 및 LiveServerTestCase 사용

--- superlists/lists/admin.py
+++ superlists/lists/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from lists.models import Item
 
 # Register your models here.
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 
 from lists.models import Item
 
-# Create your views here.
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
