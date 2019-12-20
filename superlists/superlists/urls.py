100e03b765796553206d91e95cc168717b81f678
JichangJang <wlckd90@gmail.com>
2019-12-20 21:32:20 +0900

    신규 URL 추가
    작업 목록을 추가하는 URL 추가

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -11,7 +11,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))s
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -21,4 +21,5 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('admin/', admin.site.urls),
     path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
+    path('lists/new', views.new_list, name='new_list'),
 ]
